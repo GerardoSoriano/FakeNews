4ec631adfcf9547610bf5f419e0e1f3efc37f533
Dario Valdes Banda <ruben_miklo@hotmail.com>
2017-09-29 22:46:20 -0500

    Se agrego landing page

---
+++ public/home.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Feik News</title>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+  <link href="css/clean-blog.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="home.html">Feik News</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fa fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="mainpage.php">Portada</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="registro-login.php">Registrate/Inicia sesión</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Feik News</h1>
+            <span class="subheading">Noticias de lo más irrelevante del mundo</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview">
+          <a href="#">
+            <h2 class="post-title">
+              Chabelo: el alfa y el omega
+            </h2>
+            <h3 class="post-subtitle">
+              Nadie sabe cuando nació ni mucho menos cuando morirá
+            </h3>
+          </a>
+          <p class="post-meta">Publicado por
+            <a href="#">Lee Morales</a>
+          24 Septiembre de 2017</p>
+        </div>
+        <hr>
+       <div class="post-preview">
+          <a href="#">
+            <h2 class="post-title">
+              Nominado al Premio Nobel de Física fan de Rick and Morty 
+            </h2>
+            <h3 class="post-subtitle">
+              La gusta mucho la física pero su pasión es Rick and Morty dice...
+            </h3>
+          </a>
+          <p class="post-meta">Publicado por
+            <a href="#">Lee Morales</a>
+          24 Septiembre de 2017</p>
+        </div>
+        <hr>
+        <div class="post-preview">
+          <a href="#">
+            <h2 class="post-title">
+              La juventud postmodernista en crisis
+            </h2>
+            <h3 class="post-subtitle">
+              Se debaten si continuan con la disyuntiva existencialista o hacerse feministas
+            </h3>
+          </a>
+          <p class="post-meta">Publicado por
+            <a href="#">Lee Morales</a>
+          24 Septiembre de 2017</p>
+        </div>
+        <!-- Pager
+        <div class="clearfix">
+          <a class="btn btn-secondary float-right" href="#">Older Posts &rarr;</a>
+        </div> -->
+      </div>
+    </div>
+  </div>
+  <hr>
+
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="https://github.com/GerardoSoriano/FakeNews" target="_blank">
+                <span class="fa-stack fa-lg">
+                  <i class="fa fa-circle fa-stack-2x"></i>
+                  <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p class="copyright text-muted">
+            &copy; Proyecto realizado por Dario Valdes y Gerardo Soriano
+          </p>
+          <p class="copyright text-muted">Programación y Apliaciones para la Web</p>
+          <p class="copyright text-muted">Lic. Multimedia y Animacion Digital | FCFM | UANL </p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/popper/popper.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+</body>
+</html>
